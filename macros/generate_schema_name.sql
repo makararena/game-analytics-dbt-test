a0a15f25b5c8144a8dd6fa8baebf69c5416a32f4
Makar Charviakou <m.charviakou@rubylabs.com>
2026-03-01 13:16:08 +0100

    fix: refactor generate_schema_name macro for improved clarity and structure, add ci case

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,29 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
-    {%- elif custom_schema_name | trim | lower == 'staging' -%}
-        {{ var('staging_schema', 'STAGING') }}
-    {%- elif custom_schema_name | trim | lower == 'marts' -%}
-        {{ var('marts_schema', 'MARTS') }}
+
+    {%- set base_schema = target.schema -%}
+
+    {# ---- CI ENV ---- #}
+    {%- if target.name == 'ci' -%}
+
+        {%- if custom_schema_name is none -%}
+            {{ base_schema }}
+        {%- else -%}
+            {{ base_schema }}_{{ custom_schema_name | trim | upper }}
+        {%- endif -%}
+
+    {# ---- NON-CI ENV ---- #}
     {%- else -%}
-        {{ target.schema }}_{{ custom_schema_name | trim }}
+
+        {%- if custom_schema_name is none -%}
+            {{ base_schema }}
+        {%- elif custom_schema_name | trim | lower == 'staging' -%}
+            {{ var('staging_schema', 'STAGING') }}
+        {%- elif custom_schema_name | trim | lower == 'marts' -%}
+            {{ var('marts_schema', 'MARTS') }}
+        {%- else -%}
+            {{ base_schema }}_{{ custom_schema_name | trim | upper }}
+        {%- endif -%}
+
     {%- endif -%}
+
 {%- endmacro %}
